2f0a7b33a57f4ca7e062fe906a4d090cf4fff0d2
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-13 20:01:36 -0600

    feat:se agrego la accione para cerrar sesion

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/HomeController.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/HomeController.java
@@ -2,6 +2,7 @@ package com.kevinjuarez.Sistema_Venta.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +16,10 @@ public class HomeController {
     @GetMapping("/home-cliente")
     public String mostrarCliente(){
         return "cliente";
+    }
+
+    @GetMapping("/cerrar-sesion")
+    public String redirectIndex(){
+        return ("indext");
     }
 }
