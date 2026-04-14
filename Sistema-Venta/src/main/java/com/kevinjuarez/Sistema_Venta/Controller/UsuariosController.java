ae30b88ee327163bcdce0d669f823110abdde064
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-13 19:25:59 -0600

    feat:se subio el controlador de la vista usuarios

---
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/UsuariosController.java
@@ -0,0 +1,12 @@
+package com.kevinjuarez.Sistema_Venta.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuariosController {
+    @GetMapping("/usuarios")
+    public String mostrarUsuarios(){
+        return "usuarios";
+    }
+}
