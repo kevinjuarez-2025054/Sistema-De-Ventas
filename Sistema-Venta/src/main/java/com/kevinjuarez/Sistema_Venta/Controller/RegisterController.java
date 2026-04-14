63e95199aa620b06158b95e4dbc4b59a9e6c2919
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-13 19:00:07 -0600

    feat: se creo el login

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/RegisterController.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/RegisterController.java
@@ -1,4 +1,18 @@
 package com.kevinjuarez.Sistema_Venta.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class RegisterController {
+    @GetMapping("/register")
+    public String mostrarRegister(){
+        return "redirect:/register";
+    }
+
+    @PostMapping("register-login")
+    public String registrarUsuario(){
+        return "redirect:/index";
+    }
 }
