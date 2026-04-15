06d15e4690cd91a992121e2b749846146a3b9a0f
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-15 08:24:18 -0600

    feat:se agregaron funciones para registrar

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/RegisterController.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/RegisterController.java
@@ -1,18 +1,42 @@
 package com.kevinjuarez.Sistema_Venta.Controller;
 
+import com.kevinjuarez.Sistema_Venta.Entity.Usuario;
+import com.kevinjuarez.Sistema_Venta.Service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegisterController {
+    @Autowired
+    private UsuarioService service;
+
     @GetMapping("/register")
     public String mostrarRegister(){
         return "register";
     }
 
+    @PostMapping("/registrar")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        @RequestParam String email,
+                        Model model) {
+
+        Usuario u = service.registrar(username, password,email);
+
+        if (u != null) {
+            return "redirect:/register-login";
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas");
+            return "login";
+        }
+    }
+
     @PostMapping("register-login")
     public String registrarUsuario(){
-        return "redirect:/index";
+        return "login";
     }
 }
