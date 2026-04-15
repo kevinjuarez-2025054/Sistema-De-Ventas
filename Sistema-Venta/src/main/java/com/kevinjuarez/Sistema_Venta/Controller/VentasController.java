ee9a5d9a07e3bdd604deefcf1ad663e938ffb0f3
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-15 10:49:08 -0600

    feat:se agrego metodo para listar y eliminar

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/VentasController.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/VentasController.java
@@ -1,12 +1,31 @@
 package com.kevinjuarez.Sistema_Venta.Controller;
 
+import com.kevinjuarez.Sistema_Venta.Entity.Cliente;
+import com.kevinjuarez.Sistema_Venta.Entity.Usuario;
+import com.kevinjuarez.Sistema_Venta.Service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class VentasController {
+    @Autowired
+    private ClienteService service;
+
     @GetMapping("/ventas")
-    public String mostrarVentas(){
+    public String mostrarVentas(Model model){
+        List<Cliente> lista = service.getAllCliente();
+        model.addAttribute("ventas",lista);
         return "ventas";
+    }
+
+    @GetMapping("/eliminar-cliente/{id}")
+    public String eliminarCliente(@PathVariable int id) {
+        service.deleteCliente(id);
+        return "redirect:/ventas";
     }
 }
