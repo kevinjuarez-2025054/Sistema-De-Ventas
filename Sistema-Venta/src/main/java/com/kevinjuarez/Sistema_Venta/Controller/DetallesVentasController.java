28e5434479b5b2db78c76fa3d67bd520dfde87f1
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-15 15:29:51 -0600

    feat: se agrego metodos listar y elimiar a la clase DetallesVentasController.java

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/DetallesVentasController.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/DetallesVentasController.java
@@ -1,12 +1,31 @@
 package com.kevinjuarez.Sistema_Venta.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kevinjuarez.Sistema_Venta.Entity.DetalleVentas;
+import com.kevinjuarez.Sistema_Venta.Service.DetalleVentaServiceImplements;
 
 @Controller
 public class DetallesVentasController {
+    @Autowired
+    private DetalleVentaServiceImplements service;
+
     @GetMapping("/detalleventas")
-    public String mostrarDetalleVentas(){
+    public String mostrarDetalleVentas(Model model){
+        List<DetalleVentas> lista = service.getAll();
+        model.addAttribute("detalleventas",lista);
         return "detalleventas";
+    }
+
+    @GetMapping("/eliminar-detalleventas/{id}")
+    public String eliminarDetalleVentas(@PathVariable int id) {
+        service.deleteDetalleVenta(id);
+        return "redirect:/detalleventas";
     }
 }
