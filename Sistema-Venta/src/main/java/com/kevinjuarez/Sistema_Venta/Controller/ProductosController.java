cc38601c72cb11388f11adb024ed159c53f6dd29
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-15 18:12:17 -0600

    feat: se agrego metodo listar y eliminar

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/ProductosController.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/ProductosController.java
@@ -1,12 +1,31 @@
 package com.kevinjuarez.Sistema_Venta.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kevinjuarez.Sistema_Venta.Entity.Producto;
+import com.kevinjuarez.Sistema_Venta.Service.ProductoServiceImplements;
 
 @Controller
 public class ProductosController {
+    @Autowired
+    private ProductoServiceImplements service;
+
     @GetMapping("/productos")
-    public String mostrarProductos(){
+    public String mostrarProductos(Model model){
+        List<Producto> lista = service.getAllProducto();
+        model.addAttribute("productos", lista);
         return "productos";
+    }
+
+    @GetMapping("/eliminar-producto/{id}")
+    public String eliminarProducto(@PathVariable int id) {
+        service.deleteProducto(id);
+        return "redirect:/productos";
     }
 }
