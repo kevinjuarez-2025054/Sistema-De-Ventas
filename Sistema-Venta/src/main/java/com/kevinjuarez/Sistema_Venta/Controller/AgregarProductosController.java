828f589cd8c2364e2e2191fb308ea040dce0e5b7
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-15 23:31:13 -0600

    feat: se subio agregar producto

---
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/AgregarProductosController.java
@@ -0,0 +1,31 @@
+package com.kevinjuarez.Sistema_Venta.Controller;
+
+import com.kevinjuarez.Sistema_Venta.Entity.Producto;
+import com.kevinjuarez.Sistema_Venta.Service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AgregarProductosController {
+
+    @Autowired
+    private ProductoService service;
+
+    @GetMapping("/agregarProducto")
+    public String agregarProducto(Model model){
+        model.addAttribute("producto", new Producto());
+        return "agregarProducto";
+    }
+
+    @PostMapping("/guardarProducto")
+    public String guardarProducto(@ModelAttribute Producto producto){
+
+        producto.setEstado(1);
+
+        service.saveProducto(producto);
+
+        return "redirect:/producto";
+    }
+}
