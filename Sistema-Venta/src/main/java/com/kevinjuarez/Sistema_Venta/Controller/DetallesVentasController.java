c5025c08fec79e5394d3ce1a6b63ccde5eac8bad
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-17 20:44:18 -0600

    feat:Se subio editar de detallesVentas

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/DetallesVentasController.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Controller/DetallesVentasController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.kevinjuarez.Sistema_Venta.Entity.DetalleVentas;
 import com.kevinjuarez.Sistema_Venta.Service.DetalleVentaServiceImplements;
@@ -26,6 +28,22 @@ public class DetallesVentasController {
     @GetMapping("/eliminar-detalleventas/{id}")
     public String eliminarDetalleVentas(@PathVariable int id) {
         service.deleteDetalleVenta(id);
+        return "redirect:/detalleventas";
+    }
+
+    @GetMapping("/editarDetalleVenta/{id}")
+    public String formularioEditar(@PathVariable Integer id, Model model) {
+        model.addAttribute("detalleVenta", service.getById(id));
+        return "editarDetalleVenta";
+    }
+
+    @PostMapping("/actualizarDetalleVenta")
+    public String actualizar(@ModelAttribute DetalleVentas detalleVenta) {
+        DetalleVentas original = service.getById(detalleVenta.getCodigoDetalleVenta());
+        original.setCantidad(detalleVenta.getCantidad());
+        original.setPrecioUnitario(detalleVenta.getPrecioUnitario());
+        original.setSubtotal(detalleVenta.getSubtotal());
+        service.updateDetalleVenta(original.getCodigoDetalleVenta(), original);
         return "redirect:/detalleventas";
     }
 }
