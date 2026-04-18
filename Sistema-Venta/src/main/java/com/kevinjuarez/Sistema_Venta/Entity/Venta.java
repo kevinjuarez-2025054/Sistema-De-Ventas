6958295f5406bb246dc4ea543cdd3161784e0982
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-17 21:13:29 -0600

    refactor: se realizaron cambios en Venta.java

--- Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Entity/Venta.java
+++ Sistema-Venta/src/main/java/com/kevinjuarez/Sistema_Venta/Entity/Venta.java
@@ -3,7 +3,6 @@ package com.kevinjuarez.Sistema_Venta.Entity;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Entity
 @Table(name = "Ventas")
